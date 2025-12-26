8ac7b440909c8e2c8221bd1641e879e6b9e94da7
nimrod-ambetsa <n.ambetsa21@gmail.com>
2025-12-27 01:21:42 +0300

    fix: header items misbehaviour

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const HeaderComponent = () => {
   return (
     <div>
-      <div className="fixed w-full top-0 left-0">
-        <p>Refine Online stores</p>
+      <div className="bg-green-500 w-full h-20 text-xl text-white flex mx-auto px-4 font-semibold items-center justify-start uppercase">
+        <p className="">Refine stores</p>
       </div>
+      <Menu></Menu>
     </div>
   );
 };
