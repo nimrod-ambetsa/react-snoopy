e8de01a0535197cdb7fa1bcb75680365cf51966c
nimrod-ambetsa <n.ambetsa21@gmail.com>
2025-12-27 13:43:00 +0300

    feat: homepage component added

---
+++ src/components/SidePanel.jsx
@@ -0,0 +1,19 @@
+export default function SidePanel(params) {
+  return (
+    <div>
+      <div>
+        <h2>Categories</h2>
+      </div>
+      <div>
+        <ul>
+          <li>General supplies</li>
+          <li>Beddings</li>
+          <li>Kitchen wear</li>
+          <li>Mens shoes</li>
+          <li>Women shoes</li>
+          <li>Children shoes</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,18 @@
 import HeaderComponent from "../components/HeaderComponent";
+import SidePanel from "../components/SidePanel";
+import homeImage from "../../public/images/unrivaledmarijuana-aUe9RCFFcjU-unsplash.jpg";
 
 const HomePage = () => {
   return (
     <div>
       <HeaderComponent />
-      <div>
-        <p>welcome to our stores</p>
+      <div className="flex w-full m-auto h-9/12">
+        <div className="w-1/6 mx-auto px-2">
+          <SidePanel />
+        </div>
+        <div className="w-5/6">
+          <img className="w-full m-auto" src={homeImage} alt="home image" />
+        </div>
       </div>
     </div>
   );
